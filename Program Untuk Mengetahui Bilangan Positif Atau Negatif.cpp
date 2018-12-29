c5fe727eaa5934783cd5d4a3db12dca645fcb443
RiyanSinaga007 <45660151+RiyanSinaga007@users.noreply.github.com>
2018-12-30 01:23:27 +0700

    Add files via upload

---
+++ Program Untuk Mengetahui Bilangan Positif Atau Negatif.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int x;
+	printf("masukan bilangan :");
+	scanf("%d",&x);
+	if(x>=0)
+	{
+		printf("%d adalah bilangan positif\n\n,x");
+		
+	}
+	else
+	{
+		printf("%d adalah bilangan negatif\n\n,x");
+		
+	}
+	printf("PAUSE");
+}
